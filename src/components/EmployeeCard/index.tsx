97b48a70447b27597db085d3a1451c7c7b3be9e9
AkramBadah <akram.badah@gmail.com>
2022-01-30 18:27:24 +0300

    Updated the UI of the employee card

--- src/components/EmployeeCard/index.tsx
+++ src/components/EmployeeCard/index.tsx
@@ -6,22 +6,23 @@ import StatesList from '../StatesList';
 import EmployeeImage from '../../assets/imgs/employee.png'
 
 import './EmployeeCard.scss';
-
 interface Props {
     employee: Employee
 }
 
 const EmployeeCard: FC<Props> = ({ employee }) => {
   return (
-    <div className='empolyee-card mb-3' key={employee.id}>
-        <div className='border rounded-corners overflow-hidden d-flex flex-column'>
-            <img src={EmployeeImage} alt='employee' className='w-100' />
-            <div className='border-top p-3 bg-white'>
-                <h3 className='mt-0 mb-2'>{employee.name}</h3>
-                <p className='my-0'>{employee.description}</p>
+    <div className='mb-3' key={employee.id}>
+      <div className='employee-card d-flex border rounded-corners overflow-hidden'>
+          <img src={EmployeeImage} alt='employee' width="214" height='200' />
+          <div className='d-flex flex-column justify-content-between w-100'>
+            <div className='p-3 bg-white'>
+              <h3 className='mt-0 mb-2'>{employee.name}</h3>
+              <p className='my-0'>{employee.description}</p>
             </div>
             <StatesList employee={employee} />
-        </div>
+          </div>
+      </div>
     </div>
   );
 };
